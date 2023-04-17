da46d7f3635b4b9c4ef927b25dae6b14bbc9ecb8
jangdave <jangdave@naver.com>
2023-04-17 13:22:03 +0900

    4/17
    손님 캐릭터 에셋 추가
    의자 클래스 추가
    손님 애니메이션 클래스 추가
    손님 FSM 추가

---
+++ Source/BarSim/Private/Chair.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Chair.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AChair::AChair()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("boxComp"));
+	SetRootComponent(boxComp);
+
+	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("meshComp"));
+	meshComp->SetupAttachment(boxComp);
+}
+
+// Called when the game starts or when spawned
+void AChair::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AChair::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/BarSim/Public/CustomerAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CustomerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BARSIM_API UCustomerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
