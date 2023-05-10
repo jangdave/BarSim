c7da86b605dc8624c0d052495c0e739c87088737
Kimrral <kimhhyunjjun@gmail.com>
2023-05-10 17:19:54 +0900

    Feat : Player Character Casting Grab Function
    Feat : Player Character Casting Grab Function

---
+++ Source/BarSim/Public/BarGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "BarGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BARSIM_API ABarGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
