34b9bb23c67fb9001792ff38f787657eeb16bded
Kimrral <kimhhyunjjun@gmail.com>
2023-05-25 17:36:02 +0900

    Feat : Info Widget Disappear with Animation(Right Hand)
    Feat : Info Widget Disappear with Animation(Right Hand)

--- Source/BarSim/Private/GraspingHandRealistic.cpp
+++ Source/BarSim/Private/GraspingHandRealistic.cpp
@@ -96,157 +96,242 @@ void AGraspingHandRealistic::OnOverlap(UPrimitiveComponent* OverlappedComponent,
 	if(huchuTong)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("집게"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
 		}), 1, false);
 	}
 	else if(barSpoon)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("바 스푼"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(tablet)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("태블릿"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(coaster)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("코스터"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(strainer)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("스트레이너"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(shakerStrainer)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("쉐이커 스트레이너"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(shakerLid)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("쉐이커 뚜껑"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(shaker)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("쉐이커"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(mixingGlass)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("믹싱 글래스"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(iceCube)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("얼음"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(halfSlicedLime)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("라임"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(slicedLime)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("라임"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(slicedLimeVat)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("라임 컨테이너"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(halfSlicedLimeVat)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("라임 컨테이너"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(iceCubeVat)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("아이스 컨테이너"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(martiniCup)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("마티니 컵"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 	else if(cup)
 	{
 		GetWorldTimerManager().ClearTimer(widgetDestroyHandle);
+		if(actorInfoWidget->IsAnimationPlaying(actorInfoWidget->Disappearing))
+		{
+			actorInfoWidget->StopAnimation(actorInfoWidget->Disappearing);
+		}
 		actorInfoWidget->ActorInfo->SetText(FText::FromString(FString::Printf(TEXT("글래스 컵"))));
 		actorInfoWidgetComp->SetVisibility(true);
+		actorInfoWidget->PlayAnimation(actorInfoWidget->Appearing);
 		GetWorldTimerManager().SetTimer(widgetDestroyHandle, FTimerDelegate::CreateLambda([this]()->void
 		{
-			actorInfoWidgetComp->SetVisibility(false);
-		}), 1, false);	}
+			actorInfoWidget->PlayAnimation(actorInfoWidget->Disappearing);
+		}), 1, false); }
 
 	
 }
