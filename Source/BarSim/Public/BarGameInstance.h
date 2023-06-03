52ecc133c6dcda285b2a570d2c0ded5ef07163d4
jangdave <jangdave@naver.com>
2023-06-03 11:50:01 +0900

    6/3

--- Source/BarSim/Public/BarGameInstance.h
+++ Source/BarSim/Public/BarGameInstance.h
@@ -32,9 +32,9 @@ public:
 
 	// 날짜
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	int32 checkDayCount; // 0
+	int32 checkDayCount = 2; // 0
 
 	// 게임모드 체크 불
 	UPROPERTY()
-	bool bCheckGameMode; // false
+	bool bCheckGameMode = true; // false
 };
