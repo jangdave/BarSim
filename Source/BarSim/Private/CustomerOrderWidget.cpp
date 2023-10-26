6d8443c13a336ff14cef0c53e1a81817c13b67d6
Kimrral <kimhhyunjjun@gmail.com>
2023-10-26 10:36:55 +0900

    Feat : Script Eng Version2

--- Source/BarSim/Private/CustomerOrderWidget.cpp
+++ Source/BarSim/Private/CustomerOrderWidget.cpp
@@ -633,94 +633,94 @@ void UCustomerOrderWidget::SetLanguage()
 			womanScriptC.Emplace(FString::Printf(TEXT("Martini."))); // 2
 			womanScriptC.Emplace(FString::Printf(TEXT("Negroni."))); // 3
 			// 한번
-			womanScriptC.Emplace(FString::Printf(TEXT("진라임 달라니까?"))); // 4
-			womanScriptC.Emplace(FString::Printf(TEXT("다이키리 달라니까?"))); // 5
-			womanScriptC.Emplace(FString::Printf(TEXT("마티니 달라니까?"))); // 6
-			womanScriptC.Emplace(FString::Printf(TEXT("네그로니 달라니까?"))); // 7
+			womanScriptC.Emplace(FString::Printf(TEXT("Jin lime Please?"))); // 4
+			womanScriptC.Emplace(FString::Printf(TEXT("Daiquiri Please?"))); // 5
+			womanScriptC.Emplace(FString::Printf(TEXT("Martini Please?"))); // 6
+			womanScriptC.Emplace(FString::Printf(TEXT("Negroni Please?"))); // 7
 			// 두번
-			womanScriptC.Emplace(FString::Printf(TEXT("다신 오나봐라"))); // 8
-			womanScriptC.Emplace(FString::Printf(TEXT("이 정도면 마실만 하네"))); // 9
-			womanScriptC.Emplace(FString::Printf(TEXT("이게 도대체 무슨 맛이야..."))); // 10
+			womanScriptC.Emplace(FString::Printf(TEXT("I'll never come back."))); // 8
+			womanScriptC.Emplace(FString::Printf(TEXT("This is good enough."))); // 9
+			womanScriptC.Emplace(FString::Printf(TEXT("It tastes terrible..."))); // 10
 
 			// 여성 D
 			// 주문
-			womanScriptD.Emplace(FString::Printf(TEXT("진라임...주세요"))); // 0
-			womanScriptD.Emplace(FString::Printf(TEXT("다이키리...주세요"))); // 1
-			womanScriptD.Emplace(FString::Printf(TEXT("마티니...주세요"))); // 2
-			womanScriptD.Emplace(FString::Printf(TEXT("네그로니...주세요"))); // 3
+			womanScriptD.Emplace(FString::Printf(TEXT("Please give me Jin lime."))); // 0
+			womanScriptD.Emplace(FString::Printf(TEXT("Please give me Daiquiri."))); // 1
+			womanScriptD.Emplace(FString::Printf(TEXT("Please give me Martini."))); // 2
+			womanScriptD.Emplace(FString::Printf(TEXT("Please give me Negroni."))); // 3
 			// 한번
-			womanScriptD.Emplace(FString::Printf(TEXT("진라임 아닌거 같아요..."))); // 4
-			womanScriptD.Emplace(FString::Printf(TEXT("다이키리 아닌거 같아요..."))); // 5
-			womanScriptD.Emplace(FString::Printf(TEXT("마티니 아닌거 같아요..."))); // 6
-			womanScriptD.Emplace(FString::Printf(TEXT("네그로니 아닌거 같아요..."))); // 7
+			womanScriptD.Emplace(FString::Printf(TEXT("I don't think it's a Jin lime..."))); // 4
+			womanScriptD.Emplace(FString::Printf(TEXT("I don't think it's a Daiquiri..."))); // 5
+			womanScriptD.Emplace(FString::Printf(TEXT("I don't think it's a Martini..."))); // 6
+			womanScriptD.Emplace(FString::Printf(TEXT("I don't think it's a Negroni..."))); // 7
 			// 두번
-			womanScriptD.Emplace(FString::Printf(TEXT("다음에 올게요..."))); // 8
-			womanScriptD.Emplace(FString::Printf(TEXT("감사합니다..."))); // 9
-			womanScriptD.Emplace(FString::Printf(TEXT("음...좀 이상하네요"))); // 10
+			womanScriptD.Emplace(FString::Printf(TEXT("I'll be back next time..."))); // 8
+			womanScriptD.Emplace(FString::Printf(TEXT("Thank you..."))); // 9
+			womanScriptD.Emplace(FString::Printf(TEXT("Well, it's weird..."))); // 10
 
 			// 남성 A
 			// 주문
-			manScriptA.Emplace(FString::Printf(TEXT("진라임 주세요"))); // 0
-			manScriptA.Emplace(FString::Printf(TEXT("다이키리 주세요"))); // 1
-			manScriptA.Emplace(FString::Printf(TEXT("마티니 주세요"))); // 2
-			manScriptA.Emplace(FString::Printf(TEXT("네그로니 주세요"))); // 3
+			manScriptA.Emplace(FString::Printf(TEXT("Jin lime please."))); // 0
+			manScriptA.Emplace(FString::Printf(TEXT("Daiquiri please."))); // 1
+			manScriptA.Emplace(FString::Printf(TEXT("Martini please."))); // 2
+			manScriptA.Emplace(FString::Printf(TEXT("Negroni please."))); // 3
 			// 한번
-			manScriptA.Emplace(FString::Printf(TEXT("주문한 진라임이 아닌거 같은데요?"))); // 4
-			manScriptA.Emplace(FString::Printf(TEXT("주문한 다이키리가 아닌거 같은데요?"))); // 5
-			manScriptA.Emplace(FString::Printf(TEXT("주문한 마티니가 아닌거 같은데요?"))); // 6
-			manScriptA.Emplace(FString::Printf(TEXT("주문한 네그로니가 아닌거 같은데요?"))); // 7
+			manScriptA.Emplace(FString::Printf(TEXT("I don't think it's Jin lime I ordered?"))); // 4
+			manScriptA.Emplace(FString::Printf(TEXT("I don't think it's Daiquiri I ordered?"))); // 5
+			manScriptA.Emplace(FString::Printf(TEXT("I don't think it's Martini I ordered?"))); // 6
+			manScriptA.Emplace(FString::Printf(TEXT("I don't think it's Negroni I ordered?"))); // 7
 			// 두번
-			manScriptA.Emplace(FString::Printf(TEXT("주문한 메뉴를 안주다니 못 참겠네요"))); // 8
-			manScriptA.Emplace(FString::Printf(TEXT("맛있네요 잘 마셨습니다"))); // 9
-			manScriptA.Emplace(FString::Printf(TEXT("음...제가 알던 맛이 아니네요"))); // 10
+			manScriptA.Emplace(FString::Printf(TEXT("I can't stand that you didn't give me what I ordered."))); // 8
+			manScriptA.Emplace(FString::Printf(TEXT("It's good. I enjoyed the drink."))); // 9
+			manScriptA.Emplace(FString::Printf(TEXT("Well, it's not the taste I knew."))); // 10
 	
 			// 남성 B
 			// 주문
-			manScriptB.Emplace(FString::Printf(TEXT("진라임 한 잔 부탁합니다"))); // 0
-			manScriptB.Emplace(FString::Printf(TEXT("다이키리 한 잔 부탁합니다"))); // 1
-			manScriptB.Emplace(FString::Printf(TEXT("마티니 한 잔 부탁합니다"))); // 2
-			manScriptB.Emplace(FString::Printf(TEXT("네그로니 한 잔 부탁합니다"))); // 3
+			manScriptB.Emplace(FString::Printf(TEXT("I'd like a glass of Jin lime, please."))); // 0
+			manScriptB.Emplace(FString::Printf(TEXT("I'd like a glass of Daiquiri, please."))); // 1
+			manScriptB.Emplace(FString::Printf(TEXT("I'd like a glass of Martini, please."))); // 2
+			manScriptB.Emplace(FString::Printf(TEXT("I'd like a glass of Negroni, please."))); // 3
 			// 한번
-			manScriptB.Emplace(FString::Printf(TEXT("제가 시킨 진라임이 아닙니다"))); // 4
-			manScriptB.Emplace(FString::Printf(TEXT("제가 시킨 다이키리가 아닙니다"))); // 5
-			manScriptB.Emplace(FString::Printf(TEXT("제가 시킨 마티니가 아닙니다"))); // 6
-			manScriptB.Emplace(FString::Printf(TEXT("제가 시킨 네그로니가 아닙니다"))); // 7
+			manScriptB.Emplace(FString::Printf(TEXT("It's not Jin lime, give what I ordered."))); // 4
+			manScriptB.Emplace(FString::Printf(TEXT("It's not Daiquiri, give what I ordered."))); // 5
+			manScriptB.Emplace(FString::Printf(TEXT("It's not Martini, give what I ordered."))); // 6
+			manScriptB.Emplace(FString::Printf(TEXT("It's not Negroni, give what I ordered."))); // 7
 			// 두번
-			manScriptB.Emplace(FString::Printf(TEXT("정말 형편 없군요 전 가겠습니다"))); // 8
-			manScriptB.Emplace(FString::Printf(TEXT("맛이 잘 잡혀있군요 좋습니다"))); // 9
-			manScriptB.Emplace(FString::Printf(TEXT("흠...밸런스가 안 맞는군요"))); // 10
+			manScriptB.Emplace(FString::Printf(TEXT("That's terrible. I'm going out."))); // 8
+			manScriptB.Emplace(FString::Printf(TEXT("It tastes good. That's nice."))); // 9
+			manScriptB.Emplace(FString::Printf(TEXT("Hmm... it's not balanced."))); // 10
 
 			// 남성 C
 			// 주문
-			manScriptC.Emplace(FString::Printf(TEXT("바텐더 진라임 한잔"))); // 0
-			manScriptC.Emplace(FString::Printf(TEXT("바텐더 다이키리 한잔"))); // 1
-			manScriptC.Emplace(FString::Printf(TEXT("바텐더 마티니 한잔"))); // 2
-			manScriptC.Emplace(FString::Printf(TEXT("바텐더 네그로니 한잔"))); // 3
+			manScriptC.Emplace(FString::Printf(TEXT("Bartender, a cup of Jin lime."))); // 0
+			manScriptC.Emplace(FString::Printf(TEXT("Bartender, a cup of Daiquiri."))); // 1
+			manScriptC.Emplace(FString::Printf(TEXT("Bartender, a cup of Martini."))); // 2
+			manScriptC.Emplace(FString::Printf(TEXT("Bartender, a cup of Negroni."))); // 3
 			// 한번
-			manScriptC.Emplace(FString::Printf(TEXT("진라임이 아닌데?"))); // 4
-			manScriptC.Emplace(FString::Printf(TEXT("다이키리 아닌데?"))); // 5
-			manScriptC.Emplace(FString::Printf(TEXT("마티니 아닌데?"))); // 6
-			manScriptC.Emplace(FString::Printf(TEXT("네그로니 아닌데?"))); // 7
+			manScriptC.Emplace(FString::Printf(TEXT("Do you think this is Jin lime?"))); // 4
+			manScriptC.Emplace(FString::Printf(TEXT("Do you think this is Daiquiri?"))); // 5
+			manScriptC.Emplace(FString::Printf(TEXT("Do you think this is Martini?"))); // 6
+			manScriptC.Emplace(FString::Printf(TEXT("Do you think this is Negroni?"))); // 7
 			// 두번
-			manScriptC.Emplace(FString::Printf(TEXT("뭐 이런 바가 있어? 나 참"))); // 8
-			manScriptC.Emplace(FString::Printf(TEXT("이 정도면 못 마실 정도는 아니군"))); // 9
-			manScriptC.Emplace(FString::Printf(TEXT("에잇 이것도 칵테일이라고 만든거야?"))); // 10
+			manScriptC.Emplace(FString::Printf(TEXT("Is there a terrible bar like this? Oh, my."))); // 8
+			manScriptC.Emplace(FString::Printf(TEXT("This is enough to drink."))); // 9
+			manScriptC.Emplace(FString::Printf(TEXT("Hey, is this a cocktail?"))); // 10
 
 			// 남성 D
 			// 주문
-			manScriptD.Emplace(FString::Printf(TEXT("진라임 가능할까요...?"))); // 0
-			manScriptD.Emplace(FString::Printf(TEXT("다이키리 가능할까요...?"))); // 1
-			manScriptD.Emplace(FString::Printf(TEXT("마티니 가능할까요...?"))); // 2
-			manScriptD.Emplace(FString::Printf(TEXT("네그로니 가능할까요...?"))); // 3
+			manScriptD.Emplace(FString::Printf(TEXT("Can you make a Jin lime?"))); // 0
+			manScriptD.Emplace(FString::Printf(TEXT("Can you make a Daiquiri?"))); // 1
+			manScriptD.Emplace(FString::Printf(TEXT("Can you make a Martini?"))); // 2
+			manScriptD.Emplace(FString::Printf(TEXT("Can you make a Negroni?"))); // 3
 			// 한번
-			manScriptD.Emplace(FString::Printf(TEXT("진라임 맞나요...?"))); // 4
-			manScriptD.Emplace(FString::Printf(TEXT("다이키리 맞나요...?"))); // 5
-			manScriptD.Emplace(FString::Printf(TEXT("마티니 맞나요...?"))); // 6
-			manScriptD.Emplace(FString::Printf(TEXT("네그로니 맞나요...?"))); // 7
+			manScriptD.Emplace(FString::Printf(TEXT("Is that Jin lime...?"))); // 4
+			manScriptD.Emplace(FString::Printf(TEXT("Is that Daiquiri...?"))); // 5
+			manScriptD.Emplace(FString::Printf(TEXT("Is that Martini...?"))); // 6
+			manScriptD.Emplace(FString::Printf(TEXT("Is that Negroni...?"))); // 7
 			// 두번
-			manScriptD.Emplace(FString::Printf(TEXT("음...좀 실망스럽네요"))); // 8
-			manScriptD.Emplace(FString::Printf(TEXT("맛있네요..."))); // 9
-			manScriptD.Emplace(FString::Printf(TEXT("음...맛이 좀 이상하네요"))); // 10
+			manScriptD.Emplace(FString::Printf(TEXT("Well, I'm a little disappointed..."))); // 8
+			manScriptD.Emplace(FString::Printf(TEXT("Oh, It's good..."))); // 9
+			manScriptD.Emplace(FString::Printf(TEXT("It tastes a bit weird..."))); // 10
 		}
 	}
 }
--- Source/BarSim/Private/PlayerDialogWidget.cpp
+++ Source/BarSim/Private/PlayerDialogWidget.cpp
@@ -170,65 +170,65 @@ void UPlayerDialogWidget::SetLanguage()
 			// 칵테일 나옴
 			playerScript1.Emplace(FString::Printf(TEXT("Here is your cocktail."))); // 4
 			playerScript1.Emplace(FString::Printf(TEXT("What a strange man...?"))); // 5
-			playerScript1.Emplace(FString::Printf(TEXT("(오랜만에 만든거 치곤 나쁘지 않았나 보다)"))); // 6
-			playerScript1.Emplace(FString::Printf(TEXT("오늘은 이만 집으로 돌아가자"))); // 7
+			playerScript1.Emplace(FString::Printf(TEXT("(I guess the cocktail wasn't bad.)"))); // 6
+			playerScript1.Emplace(FString::Printf(TEXT("Let's go home now."))); // 7
 
 			// 1번째 선택지
-			playerChoice1.Emplace(FString::Printf(TEXT("...알겠습니다"))); // 0
-			playerChoice1.Emplace(FString::Printf(TEXT("일단 앉으시죠"))); // 1
+			playerChoice1.Emplace(FString::Printf(TEXT("... All right, sir."))); // 0
+			playerChoice1.Emplace(FString::Printf(TEXT("Please sit down..."))); // 1
 
 			// 둘째날 대사
-			playerScript2.Emplace(FString::Printf(TEXT("오늘도 어찌저찌 하루를 마무리 했군"))); // 0
-			playerScript2.Emplace(FString::Printf(TEXT("설마 어제의 별난 손님은 오지 않겠지..."))); // 1
-			playerScript2.Emplace(FString::Printf(TEXT("마감하고 돌아가자"))); // 2
+			playerScript2.Emplace(FString::Printf(TEXT("Ended the day somehow."))); // 0
+			playerScript2.Emplace(FString::Printf(TEXT("Don't tell me yesterday's strange man will come..."))); // 1
+			playerScript2.Emplace(FString::Printf(TEXT("Let's close the day and go back."))); // 2
 
 			// 셋째날 대사
-			playerScript3.Emplace(FString::Printf(TEXT("손님...또 영업시간이 끝나고 오셨군요"))); // 0
-			playerScript3.Emplace(FString::Printf(TEXT("제 시간에 와주시면 안될까요?"))); // 1
-			playerScript3.Emplace(FString::Printf(TEXT("알겠습니다. 오늘까지만 받아드릴게요"))); // 2
-			playerScript3.Emplace(FString::Printf(TEXT("그럼 주문은 어떤걸로 드릴까요"))); // 3
-			playerScript3.Emplace(FString::Printf(TEXT("음...올드팔은 만들어 본 적이 없네요"))); // 4
-			playerScript3.Emplace(FString::Printf(TEXT("다음 영업일에 오시면 연습해두겠습니다"))); // 5
-			playerScript3.Emplace(FString::Printf(TEXT("오늘은 그럼 이만..."))); // 6
-			playerScript3.Emplace(FString::Printf(TEXT("이런, 레시피도 외우고 계세요?"))); // 7
-			playerScript3.Emplace(FString::Printf(TEXT("알겠습니다, 만들어 드리죠"))); // 8
-			playerScript3.Emplace(FString::Printf(TEXT("자리에 앉으시죠"))); // 9
-			playerScript3.Emplace(FString::Printf(TEXT("(정말 까다로운 손님이군...)"))); // 10
-			playerScript3.Emplace(FString::Printf(TEXT("아...역시 아버지의 단골이셨군요"))); // 11
-			playerScript3.Emplace(FString::Printf(TEXT("제 이름을 말씀드렸던가요...?"))); // 12
-			playerScript3.Emplace(FString::Printf(TEXT("아! 저한테 메일을 보내셨던 그...아버지의 친구분?"))); // 13
-			playerScript3.Emplace(FString::Printf(TEXT("아닙니다. 뭐...오랜 친구분이시면 그럴수 있죠"))); // 14
-			playerScript3.Emplace(FString::Printf(TEXT("사실 서로 연락이 안된지 오래됐고, 서로 관심도 없었습니다"))); // 15
-			playerScript3.Emplace(FString::Printf(TEXT("가게도 잠시만 운영하고 정리할 생각이라..."))); // 16
-			playerScript3.Emplace(FString::Printf(TEXT("손님, 아니 리차드 아저씨께는 죄송하게 됐네요"))); // 17
-			playerScript3.Emplace(FString::Printf(TEXT("내기요...? 어떤?"))); // 18
-			playerScript3.Emplace(FString::Printf(TEXT("그게 무슨...?"))); // 19
-			playerScript3.Emplace(FString::Printf(TEXT("아저씨가 어떤 칵테일을 만들어 달라고 할 줄 알고요?"))); // 20
-			playerScript3.Emplace(FString::Printf(TEXT("터무니 없어요... 말도 안된다구요"))); // 21
-			playerScript3.Emplace(FString::Printf(TEXT("(말이 안되는 요구이지만 지금은 마땅한 일도 없고...)"))); // 22
-			playerScript3.Emplace(FString::Printf(TEXT("...알겠습니다"))); // 23
-			playerScript3.Emplace(FString::Printf(TEXT("어쩔 수 없죠"))); // 24
-			playerScript3.Emplace(FString::Printf(TEXT("...아버지가 저를 생각하면서요? 말도 안돼요"))); // 25
-			playerScript3.Emplace(FString::Printf(TEXT("그렇게 연락 한 통 없던, 빌어먹을 아버지였는데요?"))); // 26
-			playerScript3.Emplace(FString::Printf(TEXT("이제와서 그런다고 도대체 뭐가 달라지죠?"))); // 27
-			playerScript3.Emplace(FString::Printf(TEXT("젓지 말고, 흔들어서 라... 익숙한 문장인데"))); // 28
-			playerScript3.Emplace(FString::Printf(TEXT("일단 오늘은 여기까지 하자"))); // 29
-			playerScript3.Emplace(FString::Printf(TEXT("빌어먹을 아버지..."))); // 30
-			playerScript3.Emplace(FString::Printf(TEXT("어떤 생각을 했는지 한 번 들어나보자고"))); // 31
+			playerScript3.Emplace(FString::Printf(TEXT("Sir... You're here again after business hours..."))); // 0
+			playerScript3.Emplace(FString::Printf(TEXT("Could you please come on time?"))); // 1
+			playerScript3.Emplace(FString::Printf(TEXT("All right... It's only today, Sir."))); // 2
+			playerScript3.Emplace(FString::Printf(TEXT("Then... what would you like to order?"))); // 3
+			playerScript3.Emplace(FString::Printf(TEXT("Well... I've never made Old Pal before."))); // 4
+			playerScript3.Emplace(FString::Printf(TEXT("If you come next time, I'm gonna practice it."))); // 5
+			playerScript3.Emplace(FString::Printf(TEXT("Then, today..."))); // 6
+			playerScript3.Emplace(FString::Printf(TEXT("Jeez... do you even know the recipe?"))); // 7
+			playerScript3.Emplace(FString::Printf(TEXT("Okay, Sir. I'm gonna try it for you."))); // 8
+			playerScript3.Emplace(FString::Printf(TEXT("Please have a seat."))); // 9
+			playerScript3.Emplace(FString::Printf(TEXT("(Such a picky customer...)"))); // 10
+			playerScript3.Emplace(FString::Printf(TEXT("Ah... as expected, you were my father's regular."))); // 11
+			playerScript3.Emplace(FString::Printf(TEXT("Did I... tell you my name...?"))); // 12
+			playerScript3.Emplace(FString::Printf(TEXT("Oh...! The person who sent me the email...? father's friend...?"))); // 13
+			playerScript3.Emplace(FString::Printf(TEXT("No, it's okay... that's possible..."))); // 14
+			playerScript3.Emplace(FString::Printf(TEXT("Actually, we haven't been in touch for a long time, and we haven't been interested in each other"))); // 15
+			playerScript3.Emplace(FString::Printf(TEXT("I'm gonna run the store for a while... and clean it up..."))); // 16
+			playerScript3.Emplace(FString::Printf(TEXT("I'm sorry, sir... um... Richard."))); // 17
+			playerScript3.Emplace(FString::Printf(TEXT("A bet...? for what...?"))); // 18
+			playerScript3.Emplace(FString::Printf(TEXT("What... the...?"))); // 19
+			playerScript3.Emplace(FString::Printf(TEXT("How could I know which cocktail you're gonna ask for...?"))); // 20
+			playerScript3.Emplace(FString::Printf(TEXT("Ridiculous... I can't believe it...!"))); // 21
+			playerScript3.Emplace(FString::Printf(TEXT("(It's a ridiculous demand, but there's no other way...)"))); // 22
+			playerScript3.Emplace(FString::Printf(TEXT("... I see."))); // 23
+			playerScript3.Emplace(FString::Printf(TEXT("... There's no other way..."))); // 24
+			playerScript3.Emplace(FString::Printf(TEXT("... My father thinks of me...? No way..."))); // 25
+			playerScript3.Emplace(FString::Printf(TEXT("... damn father who even didn't call me just once?"))); // 26
+			playerScript3.Emplace(FString::Printf(TEXT("What the heck does that change now?"))); // 27
+			playerScript3.Emplace(FString::Printf(TEXT("Shaken, Not Stirred... it sounds familiar..."))); // 28
+			playerScript3.Emplace(FString::Printf(TEXT("Let's stop here for today."))); // 29
+			playerScript3.Emplace(FString::Printf(TEXT("Damn father..."))); // 30
+			playerScript3.Emplace(FString::Printf(TEXT("Let me just hear what the heck you thought."))); // 31
 	
 			// 2번째 선택지
-			playerChoice2.Emplace(FString::Printf(TEXT("알겠습니다"))); // 0
-			playerChoice2.Emplace(FString::Printf(TEXT("앉으시죠"))); // 1
+			playerChoice2.Emplace(FString::Printf(TEXT("Okay, Sir."))); // 0
+			playerChoice2.Emplace(FString::Printf(TEXT("Please have a seat."))); // 1
 
 			// 3번째 선택지
-			playerChoice3.Emplace(FString::Printf(TEXT("...알겠습니다"))); // 0
-			playerChoice3.Emplace(FString::Printf(TEXT("어쩔 수 없죠"))); // 1
+			playerChoice3.Emplace(FString::Printf(TEXT("...I see"))); // 0
+			playerChoice3.Emplace(FString::Printf(TEXT("... There's no other way"))); // 1
 
 			// 시작 대사
-			playerStartScript.Emplace(FString::Printf(TEXT("문을 열고 바 올드 팔로 이동하세요"))); // 0
-			playerStartScript.Emplace(FString::Printf(TEXT("여기가 올드팔...이군"))); // 1
-			playerStartScript.Emplace(FString::Printf(TEXT("어제 온 손님은 오늘 안오겠지. 문을 열고 내려가자"))); // 2
-			playerStartScript.Emplace(FString::Printf(TEXT("3일째가 되니 슬슬 익숙해지는군. 문을 열고 내려가자"))); // 3
+			playerStartScript.Emplace(FString::Printf(TEXT("Open the door and move to Bar Old Pal."))); // 0
+			playerStartScript.Emplace(FString::Printf(TEXT("This is Old... Pal..."))); // 1
+			playerStartScript.Emplace(FString::Printf(TEXT("The customer who came yesterday won't come today... Let's open the door and go down."))); // 2
+			playerStartScript.Emplace(FString::Printf(TEXT("I'm getting used to it now that it's the third day... Let's open the door and go down."))); // 3
 		}
 	}
 }
